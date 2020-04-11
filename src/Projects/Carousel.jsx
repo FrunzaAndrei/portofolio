f20dc324ba8450b18fb5cbede2cb5d2038558c3b
Frunza <frunza.andrei.daniel@gmail.com>
2020-04-11 14:11:45 +0300

    slider component

---
+++ src/Projects/Carousel.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import titleData from "../store/titleData";
+
+const Slider = () => {
+  const styleCarousel = {
+    width: "auto",
+    height: "300px",
+  };
+  return (
+    <div class="carousel-wrapper">
+      <Carousel infiniteLoop useKeyboardArrows autoPlay showThumbs={false}>
+        {titleData.map((item) => (
+          <div>
+            <img src={item.img} style={styleCarousel} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Slider;
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Slider from "./Carousel";
 
 function Projects() {
   return (
     <div>
       <h1>Projects page</h1>
+      <div style={{ width: "600px", height: "300px" }}>
+        <Slider />
+      </div>
     </div>
   );
 }
